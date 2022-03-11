57301b4af84e0f224e46eb9dd46a584805df28c0
yusuf uğurlu <yusuf_ugurlu2015@outlook.com>
2022-03-11 21:11:38 +0300

    add automapper method (AuthorAction(AfterMap, BeforeMap ) - ReverseMap)

--- WebApplication1/AutoMapping/Mapping.cs
+++ WebApplication1/AutoMapping/Mapping.cs
@@ -3,18 +3,29 @@ using Common.Entities;
 using DataAccess.Model;
 using ViewModel.Parameters;
 using ViewModel.ViewModels;
+using WebApplication1.AutoMapping.MappingActions;
 
 namespace WebApplication1.AutoMapping
 {
+    //Bu sınıf bizim için Entity ve dto sınıfları arasında mapping tablosu görevi görmektedir. 
     public class Mapping:Profile
     {
         public Mapping()
         {
+            CreateMap<AuthorParameter, AuthorViewModel>();
             CreateMap<AuthorParameter, Author>();
             //Burada kaynaktaki entityde bulunan enumu hedeftedeki enumu string halini view aktarması
             //Propertylere custom özellikler eklenebilir.
             CreateMap<Author, AuthorParameter>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
-            CreateMap<AuthorParameter, AuthorViewModel>();
+
+            //Yukarıdaki maplamelerde kendine has özellikleri maplemek için IMappingAction interface kullanarak bunları farklı
+            //classlara bölebiliriz.
+            CreateMap<Author, AuthorViewModel>().AfterMap<AuthorAction>();
+            CreateMap<Author, AuthorViewModel>().BeforeMap<AuthorAction>();
+
+            //ReverseMap mapping işleminin iki yönlü olduğunu belirtir.
+            CreateMap<Author, AuthorViewModel>().ReverseMap();
+
         }
         
     }
---
+++ WebApplication1/AutoMapping/MappingActions/AuthorAction.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DataAccess.Model;
+using ViewModel.ViewModels;
+
+namespace WebApplication1.AutoMapping.MappingActions
+{
+    public class AuthorAction : IMappingAction<Author, AuthorViewModel>
+    {
+        public void Process(Author source, AuthorViewModel destination, ResolutionContext context)
+        {
+            destination.Genre = source.GenreId.ToString();
+        }
+    }
+}
